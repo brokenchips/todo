fa14f7f7df494d54f682b2ae9b251c283cbf68c0
Giovanni Rosa <brokenchips@gmail.com>
2018-05-13 18:50:15 +0200

    Server cleanup

--- code/src/Server.js
+++ code/src/Server.js
@@ -2,14 +2,11 @@ const Express = require('express');
 const App = Express();
 const Router = Express.Router();
 const Path = require('path');
-var Parser = require('body-parser');
-
-
-
-var cors = require('cors');
-
+const Parser = require('body-parser');
+const cors = require('cors');
 const port = 3333;
 
+// APP DATA
 var lastId = 1;
 var todoList = {
      data:[
@@ -31,14 +28,14 @@ var pushTodo = function(message){
         "message":message
     }
     todoList['data'].push(item);
-    
 };
 
 App.use(cors());
 App.use(Parser.urlencoded({ extended: true }));
 
+// GET 
 App.get('/data', (req, res) => {
-    console.log(`data requested ${port}`)
+    console.log(`GET requested on port ${port}`)
     res.set('Content-Type', 'application/json');
     var response = {
         status : "OK",
@@ -47,23 +44,17 @@ App.get('/data', (req, res) => {
     res.send(response)
 });
 
-App.get('/increase/', (req, res) => {
-    var message = 'last message';
-    pushTodo(message);
-    res.send(`added message: ` + message)
-});
-
+// POST 
 App.post('/save', (req, res) => {
-
+    console.log(`POST requested on port ${port}`)
     if (req.body && req.body.message && req.body.message!='') {
         var message = unescape(req.body.message);
         pushTodo(message);
-        console.log("Recieved a new message: " + message);
+        // console.log("Recieved a new message: " + message);
         res.json({status : 'OK' , message: `Data saved ${message}`});
     } else {
         res.json({status : 'Fail' , message: `ERROR! Data not saved - Try again later`});
     }
-
 });
 
 App.listen(port, () => console.log(`Listening on port ${port}`));
